ffd64898604856389701d1deb2b84206157cbd83
Thanupriyan Balarajah <132061353+Thanupriyan1999@users.noreply.github.com>
2024-08-07 16:06:53 +0530

    change

--- WindowsFormsApp1/MainForm.Designer.cs
+++ WindowsFormsApp1/MainForm.Designer.cs
@@ -132,7 +132,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(53, 133);
+            this.label3.Location = new System.Drawing.Point(53, 137);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(106, 18);
             this.label3.TabIndex = 3;
