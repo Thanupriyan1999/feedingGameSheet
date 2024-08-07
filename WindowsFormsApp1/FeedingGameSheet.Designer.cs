45dca42bc02f20ea9513f51723eb664d68085bdf
Thanupriyan Balarajah <132061353+Thanupriyan1999@users.noreply.github.com>
2024-08-07 20:01:06 +0530

    change

--- WindowsFormsApp1/FeedingGameSheet.Designer.cs
+++ WindowsFormsApp1/FeedingGameSheet.Designer.cs
@@ -365,7 +365,7 @@
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label16.Location = new System.Drawing.Point(548, 74);
+            this.label16.Location = new System.Drawing.Point(585, 74);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(95, 18);
             this.label16.TabIndex = 23;
